f6e7d62c043d157a7a8c9586696f63e87b9bc123
Justin Breese <jbreese00@hotmail.com>
2020-06-04 08:54:01 -0700

    cleaned up example for getPort

--- README.md
+++ README.md
@@ -2,7 +2,7 @@
 While working at Databricks, I have found many tips and tricks that customers have found valuable. So, I put them out on here so that everyone can benefit from. Welcome to Databricks Gems! There are helpful gems for the UI, API, etc.
 
 # [uploadNotebook.py](../master/uploadNotebook.py)
-This is a brief demo on how to use the Databricks REST API to upload a given file (python in this case) to your Databricks workspace
+Brief demo on how to use the Databricks REST API to upload a given file (python in this case) to your Databricks workspace
 
 ## Prerequisites
 * You have a [user access token](https://docs.databricks.com/dev-tools/api/latest/authentication.html) for your Databricks workspace 
@@ -15,6 +15,25 @@ Explain the args being passed in order:
 * Your user access token (from the prereqs)
 * The file that you want to move from your laptop to your workspace
 * The path where you want the file uploaded
+
+# [getPort.py](../master/getPort.py)
+Showing you how to get the port of a given `clusterId`
+
+## Prerequisites
+* You have a [user access token](https://docs.databricks.com/dev-tools/api/latest/authentication.html) for your Databricks workspace
+* You have a given `clusterId`
+
+## Sample command
+` python3 .\getPort.py https://demo.cloud.databricks.com <insert-your-token-here> 0318-151752-abed99`
+
+Explain the args being passed in order:
+* URL of your Databricks environment (note: no trailing `/`)
+* Your user access token (from the prereqs)
+* `clusterId` of the specific cluster
+
+## Important things to understand
+* In the Databricks World, a cluster uses a single `applicationId`
+* The port is dynamic; meaning it is a different value each time it starts up or restarts - so you may have to periodically re-run this command to get the latest port
 
 # TODO
 * Shuffle calculator
--- getPort.py
+++ getPort.py
@@ -3,17 +3,30 @@ import sys
 import time
 
 # upload files, leverage the encode64 function to send as the payload
-def request(endpoint, payload):
-    response = requests.post(
+def request(method, endpoint, payload):
+    if method == "post":    
+        response = requests.post(
+            '%s/api/1.2/%s' % (host, endpoint),
+            headers={'Authorization': 'Bearer %s' % token},
+            json = payload
+        )
+    elif method == "get":
+        response = requests.get(
         '%s/api/1.2/%s' % (host, endpoint),
         headers={'Authorization': 'Bearer %s' % token},
         json = payload
     )
+    else:
+        print("There was an error.")
+        exit()
 
     if response.status_code == 200:
         return response.json()
     else:
-        print("There was an error.")
+        print("There was an error: " + response.json()["error"])
+        # delete the context as we don't need it anymore
+        request("post", "contexts/destroy", {"contextId": "%s" % contextId, "clusterId": "%s" % clusterId})
+        exit()
 
 def getResult(endpoint):
     response = requests.get(
@@ -27,13 +40,23 @@ if __name__ == '__main__':
     token = sys.argv[2]
     clusterId = sys.argv[3]
 
-    contextId = request("contexts/create", {"language": "scala", "clusterId": "%s" % clusterId})["id"]
-    commandId = request("commands/execute", {"language": "scala", "clusterId": "%s" % clusterId,"contextId": "%s" % (contextId), "command": "spark.conf.get(\"spark.ui.port\")  + \" -- \" + spark.conf.get(\"spark.app.id\")"})["id"]
-    # wait 2 seconds so the command can register and finish
-    print("the job has started")
+    # create a context so we can run a command against it - return the contextId
+    contextId = request("post", "contexts/create", {"language": "scala", "clusterId": "%s" % clusterId})["id"]
+
+    # submit a command to the contextId from the previous step - return the commandId
+    commandId = request("post", "commands/execute", {"language": "scala", "clusterId": "%s" % clusterId,"contextId": "%s" % (contextId), "command": "spark.conf.get(\"spark.ui.port\")  + \" -- \" + spark.conf.get(\"spark.app.id\")"})["id"]
+    
+    # wait 5 seconds so the command can register and finish
+    print("Getting the port...")
     time.sleep(5)
-    portIdAppId = getResult("commands/status?clusterId=%s&contextId=%s&commandId=%s" % (clusterId, contextId, commandId))["results"]["data"]
-    print("contextId: " + contextId)
-    print("commandId: " + commandId)
+
+    # retrieve the results from the command - return the port and applicationId
+    # for some reason, it appears that this API only accepts args, so there is a different function written for it getResult()
+    portIdAppId = getResult("commands/status?clusterId=%s&contextId=%s&commandId=%s" % (clusterId, contextId, commandId))["results"]["data"].replace("res0: String = ", "")
+    
+    # delete the context as we don't need it anymore
+    print("Cleaning up...")
+    request("post", "contexts/destroy", {"contextId": "%s" % contextId, "clusterId": "%s" % clusterId})
+
+    # display the port and applicationId 
     print("portId and appId: " + portIdAppId)
- 
