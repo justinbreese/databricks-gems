c13c2a59f068a8ec7b973c8cecd71b10da268931
justinbreese <justinbreese>
2021-10-12 22:46:49 +0000

    updated reqs

--- python-docker/coolNotebook.py
+++ python-docker/coolNotebook.py
@@ -19,6 +19,10 @@ display(df)
 
 # COMMAND ----------
 
+pip install Flask
+
+# COMMAND ----------
+
 # MAGIC %md # pip freeze the env
 # MAGIC * Current limitation: we currently cannot write directly to a repo file/notebook; however we can do it for normal notebooks. 
 # MAGIC * That functionality is coming in the next 3ish months. In the interim, we have to do a quick copy paste
